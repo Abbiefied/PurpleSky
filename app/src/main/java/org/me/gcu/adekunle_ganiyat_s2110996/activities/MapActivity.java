fe1dce258bde5654261f50b85d36ecd884aa6c48
gadekunle-admin <g.adekunle@alustudent.com>
2024-04-06 03:41:12 +0400

    map functionality

--- app/src/main/java/org/me/gcu/adekunle_ganiyat_s2110996/activities/MapActivity.java
+++ app/src/main/java/org/me/gcu/adekunle_ganiyat_s2110996/activities/MapActivity.java
@@ -2,6 +2,7 @@ package org.me.gcu.adekunle_ganiyat_s2110996.activities;
 
 import android.database.Cursor;
 import android.database.MatrixCursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -19,6 +20,14 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
 
 import org.me.gcu.adekunle_ganiyat_s2110996.R;
 import org.me.gcu.adekunle_ganiyat_s2110996.data.models.CurrentWeather;
@@ -27,7 +36,9 @@ import org.me.gcu.adekunle_ganiyat_s2110996.data.repositories.WeatherRepository;
 import org.me.gcu.adekunle_ganiyat_s2110996.ui.adapters.WeatherInfoWindowAdapter;
 import org.me.gcu.adekunle_ganiyat_s2110996.utils.WeatherIconUtils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -35,6 +46,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private GoogleMap googleMap;
     private SearchView searchView;
     private SimpleCursorAdapter suggestionAdapter;
+    private HeatmapTileProvider mHeatmapTileProvider;
+    private TileOverlay mHeatmapOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -136,13 +149,20 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             Marker marker = googleMap.addMarker(markerOptions);
 
             // Zoom the map to the searched location
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7));
 
             // Fetch and display weather information for the searched location
             WeatherRepository weatherRepository = new WeatherRepository(this);
             weatherRepository.fetchCurrentWeather(locationId, new WeatherRepository.WeatherCallback<CurrentWeather>() {
                 @Override
                 public void onSuccess(CurrentWeather currentWeather) {
+                    // Get the weather icon based on temperature
+                    int iconResId = WeatherIconUtils.getWeatherIconResId(currentWeather.getTemperature());
+                    BitmapDescriptor weatherIcon = BitmapDescriptorFactory.fromResource(iconResId);
+
+                    // Update the marker icon with the weather icon
+                    marker.setIcon(weatherIcon);
+
                     // Display weather information in the info window
                     marker.setTag(locationId);
                     mWeatherData.put(locationId, currentWeather);
